c36641ab474f3b65753ce7dcee792f61ac57c290
BitBaseBit <vardygreg23@gmail.com>
2021-04-16 00:49:40 +0100

    added updated env.sh

--- envInstall.sh
+++ envInstall.sh
@@ -1,3 +1,11 @@
+sudo apt update
+sudo apt-get install clang
+sudo apt-get install gcc
+sudo apt-get install make
+sudo apt-get install python3-dev
+sudo apt-get install cmake
+
+
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
@@ -9,3 +17,5 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+sudo apt-get install fonts-powerline
