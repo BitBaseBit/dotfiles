2931a6c81776d71810ab7fdc48bb991af6bdaae3
BitBaseBit <vardygreg23@gmail.com>
2021-05-02 21:04:29 +0100

    updated vimrc

--- envInstallUnix.sh
+++ envInstallUnix.sh
@@ -1,3 +1,4 @@
+
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
