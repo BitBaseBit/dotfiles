33571bacb36f5f05a7fbadfacda4a11a3731316e
Ubuntu <ubuntu@ip-172-31-43-28.us-east-2.compute.internal>
2021-05-09 22:01:04 +0000

    updated tmux conf
    Committer: BitBaseBit <vardygreg23@gmail.com>

--- startGeth.sh
+++ startGeth.sh
@@ -1,1 +1,1 @@
-geth --syncmode=fast --cache=65536 --datadir /mnt/eth/ --ws --http --http.api personal,eth,net,web3,debug  --txlookuplimit=0
+geth --syncmode=fast --cache=65536 --datadir /mnt/datadir/ --ws --http --http.api personal,eth,net,web3,debug  --txlookuplimit=0
