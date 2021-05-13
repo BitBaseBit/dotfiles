7a2190a35da94a4a2e7309e7da2d961e80de9f98
BitBaseBit <vardygreg23@gmail.com>
2021-05-13 18:04:39 +0100

    removed old files

--- startGeth.sh
+++
@@ -1,1 +0,0 @@
-geth --syncmode=fast --cache=65536 --datadir /mnt/datadir/ --ws --http --http.api personal,eth,net,web3,debug  --txlookuplimit=0
--- test.sh
+++
@@ -1,3 +0,0 @@
-for i in openssl nvim; do
-    sudo apt-get install $i
-done
